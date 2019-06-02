ce0ebfb769f980a5353cd43053b251dbcfa0804e
Hardspyne <79159707922@yandex.ru>
2019-06-02 17:31:35 +0300

    change setting

---
+++ src/main/java/com/hardspyne/activemqtest/model/Message.java
@@ -0,0 +1,49 @@
+package com.hardspyne.activemqtest.model;
+
+public class Message {
+
+    private MessageType type;
+    private String content;
+    private String sender;
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "type=" + type +
+                ", content='" + content + '\'' +
+                ", sender='" + sender + '\'' +
+                '}';
+    }
+
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
+    }
+
+
+}
