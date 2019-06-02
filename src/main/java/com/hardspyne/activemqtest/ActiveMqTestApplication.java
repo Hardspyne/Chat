34f0f0575ac76804a2556359e96f4d7649a97499
Hardspyne <79159707922@yandex.ru>
2019-06-02 13:01:44 +0300

    create project

---
+++ src/main/java/com/hardspyne/activemqtest/ActiveMqTestApplication.java
@@ -0,0 +1,13 @@
+package com.hardspyne.activemqtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveMqTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiveMqTestApplication.class, args);
+	}
+
+}
